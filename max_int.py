39a44a725f62518b360608e7cc3251800d938a52
DonNinja <yngvileo@gmail.com>
2019-09-05 14:04:15 +0000

    added max_int.py

---
+++ max_int.py
@@ -0,0 +1,9 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+
+
+print("The maximum is", max_int)    # Do not change this line
+
+# First we have to take in number
+# Then it runs until a negative number is written into the input
+# 
